93958df0db11c2d5588907fe770340d07b4758fa
W-mseg <marco.segretario2705@gmail.com>
2022-09-19 17:28:10 +0200

    boilerplate

---
+++ module/helpers/config.mjs
@@ -0,0 +1,29 @@
+export const ANIMASIMPLIFIED = {};
+
+// Define constants here, such as:
+ANIMASIMPLIFIED.foobar = {
+  'bas': 'ANIMASIMPLIFIED.bas',
+  'bar': 'ANIMASIMPLIFIED.bar'
+};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ ANIMASIMPLIFIED.abilities = {
+  "str": "ANIMASIMPLIFIED.AbilityStr",
+  "dex": "ANIMASIMPLIFIED.AbilityDex",
+  "con": "ANIMASIMPLIFIED.AbilityCon",
+  "int": "ANIMASIMPLIFIED.AbilityInt",
+  "wis": "ANIMASIMPLIFIED.AbilityWis",
+  "cha": "ANIMASIMPLIFIED.AbilityCha"
+};
+
+ANIMASIMPLIFIED.abilityAbbreviations = {
+  "str": "ANIMASIMPLIFIED.AbilityStrAbbr",
+  "dex": "ANIMASIMPLIFIED.AbilityDexAbbr",
+  "con": "ANIMASIMPLIFIED.AbilityConAbbr",
+  "int": "ANIMASIMPLIFIED.AbilityIntAbbr",
+  "wis": "ANIMASIMPLIFIED.AbilityWisAbbr",
+  "cha": "ANIMASIMPLIFIED.AbilityChaAbbr"
+};
